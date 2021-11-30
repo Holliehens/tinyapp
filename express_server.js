07faad07bdc84043c8710839b48adc71be437702
Hollie Hensman <hensmanh@gmail.com>
2021-11-30 02:08:53 +0000

    Create new file 'views/urls_new.ejs Add GET request for /urls/new Install 'body-parser'

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -25,6 +27,15 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res)=> {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   const { shortURL } = req.params;
