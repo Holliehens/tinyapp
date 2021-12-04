ea7fee823935bc53a93baf5a5715758845b00fa1
Hollie Hensman <hensmanh@gmail.com>
2021-12-04 23:51:44 +0000

    Handle Registration Errors

--- express_server.js
+++ express_server.js
@@ -28,15 +28,15 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const username = req.cookies.username; 
-  const templateVars = { username };
+  const user_id = req.cookies.user_id; 
+  const templateVars = { user_id };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  const username = req.cookies.username;
-  const templateVars = { urls: urlDatabase, username };
-
+  const user_id = req.cookies.user_id;
+  const templateVars = { urls: urlDatabase, user: users[user_id] };
+  console.log(users);
   res.render("urls_index", templateVars);
 });
 
@@ -49,7 +49,7 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = {
     longURL: longURL,
-    // userId: userId
+     //user_id: user_id
   };
   console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
@@ -58,8 +58,9 @@ app.post("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const { shortURL } = req.params;
   const { longURL } = urlDatabase[shortURL];
-  const username = req.cookies.username; 
-  const templateVars = { shortURL, longURL, username };
+  const user_id = req.cookies.user_id; 
+  const user = users[user_id]
+  const templateVars = { shortURL, longURL, user };
 
   res.render("urls_show", templateVars);
 });
@@ -105,26 +106,26 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
+  const user_id = req.body.user_id;
 
   // cookie takes the arg of (key, value). key sets the cookie name.
-  res.cookie("username", username);
+  res.cookie("user_id", user_id);
 
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
 
   res.redirect("/urls");
 });
 
 function generateRandomString(length) {
-  var result = "";
-  var characters =
+  let result = "";
+  const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < 6; i++) {
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
@@ -132,12 +133,62 @@ function generateRandomString(length) {
 
   /// REGISTRATION PAGE ///
    app.get("/register", (req, res) => {
-  //     //template variable
-       //const templateReg =
 
-      res.render("register", { username: req.cookies.username });
+      res.render("register", { user_id: req.cookies.user_id, user: ""});
   });
 
+  /// USERS OBJECT ///
+
+  const users = { 
+    "userRandomID": {
+      id: "userRandomID", 
+      email: "user@example.com", 
+      password: "purple-monkey-dinosaur"
+    },
+   "user2RandomID": {
+      id: "user2RandomID", 
+      email: "user2@example.com", 
+      password: "dishwasher-funk"
+    }
+  }
+    const isMissingParam = (req) => {
+      if (!req.body.email || !req.body.password) {
+        return true;
+      } 
+      return false;
+    }
+    const emailExists = (email) => {
+      for (let user_id in users) {
+        if (users[user_id].email === email) {
+          return true;
+        } 
+      }
+      return false;
+    };
+
+  app.post("/register", (req, res) => {
+    if (isMissingParam(req)) {
+      return res.status(400).send("Email & Password Are Required");
+    }
+
+    if (emailExists(req.body.email)) {
+      return res.status(400).send("Email Already Exists");
+    }
+    const user_id = generateRandomString();
+    const user = {
+      id: user_id,
+      email: req.body.email,
+      password: req.body.password,
+    }
+    users[user_id] = user
+
+    
+    
+
+    res.cookie("user_id", user_id);
+    res.redirect("/urls");
+  });
+ 
 // Listener
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
